b8d2338939839f92440ea47ff3ca62a9113a24bd
Jackson Perry <jackperry2187@gmail.com>
2021-05-18 23:58:40 -0400

    Initialize Bits

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,7 @@
     let plus = $('#plus');
     let minus = $('#minus');
     let oldBits = 4;
+    let oldBytes = 0;
     
     function initialize() {
         for(let x = 0; x < bits.val(); x++) {
@@ -21,36 +22,75 @@
         }
     }
 
-    bits.change(function() {
+    const changeBits = () => {
         binaryInputs = $('#binaryInputs');
         direction = $('input[name="direction"]:checked');
-        if(direction.val() == "rl" && bits.val() > oldBits) {
-            $('<input>')
-            .attr({
-                type: 'number',
-                name: "binaryInput",
-                value: 0,
-                placeholder: 0,
-            })
-            .prependTo(binaryInputs);
+        if(bits.val() < 1) {
+            oldBits = 1;
+            bits.val(1);
+            binaryInputs.empty().append(
+                $('<input>')
+                .attr({
+                    type: 'number',
+                    name: "binaryInput",
+                    value: 0,
+                    placeholder: 0,
+                })
+            );
         }
-        else if(direction.val() == "rl" && bits.val() < oldBits) {
-            binaryInputs.children().first().remove();
+        while(bits.val() != oldBits) {
+            if(direction.val() == "rl" && bits.val() > oldBits) {
+                $('<input>')
+                .attr({
+                    type: 'number',
+                    name: "binaryInput",
+                    value: 0,
+                    placeholder: 0,
+                })
+                .prependTo(binaryInputs);
+                oldBits++;
+            }
+            else if(direction.val() == "rl" && bits.val() < oldBits) {
+                binaryInputs.children().first().remove();
+                oldBits--;
+            }
+            else if(direction.val() == "lr" && bits.val() > oldBits) {
+                $('<input>')
+                .attr({
+                    type: 'number',
+                    name: "binaryInput",
+                    value: 0,
+                    placeholder: 0,
+                })
+                .appendTo(binaryInputs);
+                oldBits++;
+            }
+            else if(direction.val() == "lr" && bits.val() < oldBits) {
+                binaryInputs.children().last().remove();
+                oldBits--;
+            }
+            bytes.val(parseInt(bits.val() / 8));
+            oldBytes = bytes.val();
         }
-        else if(direction.val() == "lr" && bits.val() > oldBits) {
-            $('<input>')
-            .attr({
-                type: 'number',
-                name: "binaryInput",
-                value: 0,
-                placeholder: 0,
-            })
-            .appendTo(binaryInputs);
+    }
+
+    bits.change(changeBits);
+
+    bytes.change(() => {
+        let bitsValue = parseInt(bits.val());
+        if(bytes.val() < 0) {
+            oldBytes = 0;
+            bytes.val(0);
+            return;
         }
-        else if(direction.val() == "lr" && bits.val() < oldBits) {
-            binaryInputs.children().last().remove();
+        if(bytes.val() > oldBytes) {
+            bits.val(bitsValue + 8);
         }
-        oldBits = bits.val();
+        else if(bytes.val() < oldBytes) {
+            bits.val(bitsValue - 8);
+        }
+        oldBytes = bytes.val();
+        changeBits();
     });
 
     initialize();
